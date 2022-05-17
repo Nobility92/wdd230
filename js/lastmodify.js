cf1f2d880e0f6ce2f96a99a1ff8c8bbb8da15f61
Nobility92 <nwa21004@byui.edu>
2022-05-17 12:21:29 +0100

    commit

--- js/lastmodify.js
+++ js/lastmodify.js
@@ -1,3 +1,3 @@
 const d = new Date();
-        let datetime = d.getFullYear()+'/'+d.getMonth()+'/'+d.getDate()+ '  ' + d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
-        document.getElementById("update").innerHTML = datetime;
+let datetime = d.getFullYear()+'/'+d.getMonth()+'/'+d.getDate()+ '  ' + d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
+document.getElementById("update").innerHTML = datetime;
