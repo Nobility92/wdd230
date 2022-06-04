408c0541905ae499f6d844f081b23e9c580d7638
Nobility92 <nwa21004@byui.edu>
2022-06-04 17:16:50 +0100

    commit

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,3 +1,24 @@
+const datefield = document.querySelector(".date");
+
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+//datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+const d = new Date();
+let datetime = d.getFullYear()+'/'+d.getMonth()+'/'+d.getDate()+ '  ' + d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
+document.getElementById("update").innerHTML = datetime;
+
+
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -6,3 +27,4 @@ function toggleMenu(){
 
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
+
