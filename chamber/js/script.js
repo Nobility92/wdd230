cf832413524c174654faf9fa05c5dd1ecf08a2f3
Nobility92 <nwa21004@byui.edu>
2022-06-21 15:56:59 +0100

    cc

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -40,3 +40,34 @@ if (day == weekday[1] || day == weekday[2]){
     }else{
         document.getElementById("banner").innerHTML = '';
     }
+
+    //for lazy load images
+    let imgload = document.querySelectorAll("img[data-src]");
+
+const imgopt = {
+    threshold: 2,
+    rootMargin: "0px 0px 50px 0px"
+};
+const loadimg = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttibute('data-src');
+    };
+};
+imgload.forEach((img) => {
+    loadimg(img);
+});
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+      });
+    }, imgopt);
+
+    imgload.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    
+        loadimg(img);
+      }
