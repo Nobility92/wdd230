3f4e2ea1a43c5179eb2658af8fddb0a5c684d322
Nobility92 <nwa21004@byui.edu>
2022-06-30 15:21:45 +0100

    c

--- lesson9/prophet.js
+++ lesson9/prophet.js
@@ -1,39 +1,36 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);
-    prophets.forEach(displayProphets);  // temporary checking for valid response and data parsing
-  });
+async function getProphets(){
+  let response = await fetch(requestURL);
+  if(response.ok){
+    let data = await response.json();
+    //console.log(data);
+    buildcards(data);
+  }else{
+    throw Error(response.statusText);
+  }
+}
 
-  const prophets = jsonObject['prophets'];
- 
-
-
-
-  function displayProphets(prophet) {
-    // Create elements to add to the document
+function buildcards(data){
+  data.prophets.forEach(prophet => {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-  
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
-  
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', prophet.imageurl);
-    h2.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
-    card.setAttribute('loading', 'lazy');
-  
-    // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
-    card.appendChild(portrait);
-  
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
-  }
-  
+    let p = document.createElement('p');
+    let image = document.createElement('img');
+
+    h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+    p.innerHTML = `Date of Birth: ${prophet.birthdate} <br>Place of Birth: <strong>${prophet.birthplace}</strong>`;
+   
+    image.setAttribute('src', prophet.imageurl);
+    image.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
+
+    card.append(h2);
+    card.append(p);
+    card.append(image);
+    cards.append (card);
+
+
+  });
+}
+  getProphets();
