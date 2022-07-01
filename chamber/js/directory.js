5782e781200131a031e22bf90d248d367c5bc923
Nobility92 <nwa21004@byui.edu>
2022-07-01 14:54:11 +0100

    c

---
+++ chamber/js/directory.js
@@ -0,0 +1,37 @@
+const direct = 'data.json';
+const company = document.querySelector('#company');
+
+async function getCompany(){
+    let response = await fetch(direct);
+    if(response.ok){
+      let data = await response.json();
+      //console.log(data);
+      buildcompany(data);
+    }else{
+      throw Error(response.statusText);
+    }
+  }
+
+  function buildcompany(data){
+    data.company.forEach(details => {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let p = document.createElement('p');
+      let image = document.createElement('img');
+
+      image.setAttribute('src', company.logo);
+      h2.innerHTML = `${company.name}`;
+      p.innerHTML = `Address: ${company.address} <br>Phone Number: ${company.phoneNumber} <br> Website: ${company.website}`;
+     
+      
+      image.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
+  
+      card.append(image);
+      card.append(h2);
+      card.append(p);
+      
+      company.append(card);
+  
+    });
+  }
+  getCompany();
