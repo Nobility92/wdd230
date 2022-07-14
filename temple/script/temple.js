91fe0961ffec8aa93c0869f230880a70221b46e7
Nobility92 <nwa21004@byui.edu>
2022-07-14 13:20:12 +0100

    c

--- temple/script/temple.js
+++ temple/script/temple.js
@@ -11,3 +11,17 @@ x.onclick = toggleMenu;
 const d = new Date();
 let datetime = d.getFullYear()+'/'+d.getMonth()+'/'+d.getDate()+ '  ' + d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
 document.getElementById("update").innerHTML = datetime;
+
+//for temple homepage display
+const temple = 'https://nobility92.github.io/wdd230/temple/temple.json';
+const temp = document.querySelector('#temple-display');
+
+async function getTemple(){
+    let response = await fetch(temple);
+    if(response.ok){
+        let data = await response.json();
+        console.log(data);
+    }else{
+        throw Error(response.statusText);
+    }
+}
