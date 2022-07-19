6b5e8815fa55e2f330f8d43dea9ad12711957ae9
Nobility92 <nwa21004@byui.edu>
2022-07-19 15:45:12 +0100

    c

--- temple/script/windchil.js
+++ temple/script/windchil.js
@@ -1,3 +1,6 @@
+
+const url = "https://api.openweathermap.org/data/2.5/onecall?lat=38.98358531123399&lon=-77.09546743732618&exclude=hourly,minutely&appid=063dbb4eea74383c891a7e335525315b&units=imperial";
+
 const weatherIcon = document.querySelector('#weather-icon');
 const temp = document.querySelector('#t');
 const wcondition = document.querySelector('#condition');
@@ -6,7 +9,6 @@ const day1  = document.querySelector('#day1');
 const day2  = document.querySelector('#day2');
 const day3  = document.querySelector('#day3');
 
-const url = "https://api.openweathermap.org/data/2.5/onecall?lat=38.98358531123399&lon=-77.09546743732618&exclude=hourly,minutely&appid=063dbb4eea74383c891a7e335525315b&units=imperial";
 
 apiFetch(url);
 
@@ -22,16 +24,17 @@ async function apiFetch(apiUrl){
 }
 function displayResults(weatherdata){
     temp.innerHTML = `<strong>${weatherdata.current.temp.toFixed(1)}</strong>`;
+    humidity.innerHTML = `${weatherdata.current.humidity.toFixed(1)}`;
     day1.innerHTML = `${weatherdata.daily[1].temp.day.toFixed(1)}`;
     day2.innerHTML = `${weatherdata.daily[2].temp.day.toFixed(1)}`;
     day3.innerHTML = `${weatherdata.daily[3].temp.day.toFixed(1)}`;
-    humidity.innerHTML = `${weatherdata.current.humidity.toFixed(1)}`;
+    
     const iconsrc = `https://openweathermap.org/img/w/${weatherdata.current.weather[0].icon}.png`;
     wcondition.innerHTML = `<strong>${weatherdata.current.weather[0].description}</strong>`;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', wcondition);
 
-    weatherIcon = wcondition;
+    weatherIcon.innerHTML = wcondition;
     
 
 }
